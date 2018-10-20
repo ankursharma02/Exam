852144431c76507bf9e36057eeca2f88ea642219
ankursharma02 <ankurs083@outlook.com>
2018-10-20 21:40:18 +0530

    asdd

---
+++ Exam/Net_pro/Server/Server1.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Server1
+    {
+        static Dictionary<string, ClientHandler> dic = new Dictionary<string, ClientHandler>();
+        static List<int> li = new List<int>();
+        static Socket s;
+        static Socket a;
+
+        static void Connection()
+        {
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234));
+            s.Listen(5);
+        }
+
+        static void Operation()
+        {
+            while (true)
+            {
+                // Accept the incoming request
+                a = s.Accept();
+                try
+                {
+
+                    byte[] Buf = new byte[255];
+                    int rec = a.Receive(Buf, 0, Buf.Length, 0);
+                    Array.Resize(ref Buf, rec);
+                    string client_name = Encoding.Default.GetString(Buf);
+                    Console.WriteLine(client_name + " Join");
+                    byte[] send = Encoding.Default.GetBytes(client_name);
+                    a.Send(send, 0, send.Length, 0);
+                    byte[] Buf1 = new byte[255];
+                    int rec1 = a.Receive(Buf1, 0, Buf1.Length, 0);
+                    Array.Resize(ref Buf1, rec1);
+                    string dis1 = Encoding.Default.GetString(Buf1);
+                    Console.WriteLine(dis1);
+                    ClientHandler mtch = new ClientHandler(a, client_name, dis1, dic);
+                    dic.Add(client_name, mtch);
+                   // li.LastIndexOf();        
+                    Thread t = new Thread(mtch.Run);
+                    Thread t1 = new Thread(mtch.Send);
+                    t.Start();
+                    t1.Start();
+                }
+                catch
+                {
+                    Console.WriteLine("Hello");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("WelCome To Chat Server");
+            Connection();
+            Operation();
+
+        }
+
+    }
+}
---
+++ Exam/ado/HandlIndust_details/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlIndust_details
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+
+        public void CreateTable()
+        {
+             //   lable1:
+            SqlConnection con = null;
+            try
+            {
+
+                    lable1:
+                // Creating Connection  
+                con = new SqlConnection("data source=.; database=My_Database; integrated security=SSPI");
+                // writing sql query 
+                
+                Console.WriteLine("Enter Company name");
+                string name = Console.ReadLine();
+                SqlCommand cm = new SqlCommand("SELECT Country_id, s_w_indust_name,h_w_comp_name FROM Industr_details where   s_w_indust_name='"+name+"'  ", con);
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+                SqlDataReader sdr = cm.ExecuteReader();
+
+                //  Console.WriteLine(sdr.HasRows);
+
+                // Iterating Data  
+                if (!sdr.HasRows)
+                {
+                    Console.WriteLine("no data found");
+                    Console.WriteLine("if want to continue again write y or close write n <y/n>");
+                    char ch = char.Parse(Console.ReadLine());
+                    if (ch.Equals('y'))
+                    {
+                        Console.Clear();
+                        cm.Dispose();
+                        sdr.Close();
+                        con.Close();
+                        
+                       
+                        goto lable1;
+                    }
+
+                }
+                else
+                {
+                 //   sdr = cm.ExecuteReader();
+                    while (sdr.Read())
+                    {
+
+                        Console.WriteLine(sdr[0] + " " + sdr[1] + " " + sdr[2]); // Displaying Record  
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong.\n" + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                Console.ReadLine();
+                con.Close();
+            }
+        }
+    }
+}
+
